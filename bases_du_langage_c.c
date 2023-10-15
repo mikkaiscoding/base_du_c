26dc02f38b94daa18e50d920d7cacc4d24e87e04
mikkaiscoding <mikka.kantinti@gmail.com>
2023-10-15 15:13:52 +0200

    information nÃecessaire

--- bases_du_langage_c.c
+++ bases_du_langage_c.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-int main(){
-    printf("Hello World !\n");  
+#include<stdio.h> // stdio.h bibliotheque Standard Input Output entrees sorties standards : permet la saisie au clavier et l'affichage a l'ecran
+int main(){ //int main : module principal, tous les programmes ont un module main. { = debut } = fin
+    printf("Hello World !\n");  // { = debut } = fin
     return 0;           
 }
